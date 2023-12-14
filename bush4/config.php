515f4785ab39b274b12bfdc0cad8d501a2122a36
zamzam0022 <zamzamanaya26@gmail.com>
2023-12-14 13:23:07 +0500

    working

--- bush4/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$connection = mysqli_connect("localhost", "root", "", "admin2");
-if(!$connection){
-    die(mysqli_connect_error());
-}
-
-
-?>
--- bush4/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('includes/header.php');
-include('includes/sidebar.php');
-include('includes/topbar.php');
-include('includes/footer.php');
-
-?>
